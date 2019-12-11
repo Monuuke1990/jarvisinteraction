14a0dced39b8c5a2673a572165aeab26033dd60f
Monuuke1990 <56351788+Monuuke1990@users.noreply.github.com>
2019-12-11 16:10:36 +0530

    Add files via upload

---
+++ build/precache-manifest.1328c4f55a0b6788167967aea6b41a50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37ca32f72838cc27979d4da91e817669",
+    "url": "/jarvisinteraction/index.html"
+  },
+  {
+    "revision": "b0390d3df6f594585b71",
+    "url": "/jarvisinteraction/static/css/main.e8047410.chunk.css"
+  },
+  {
+    "revision": "e381e0ea2ded781c4991",
+    "url": "/jarvisinteraction/static/js/2.1bf3d72d.chunk.js"
+  },
+  {
+    "revision": "b0390d3df6f594585b71",
+    "url": "/jarvisinteraction/static/js/main.5df96c82.chunk.js"
+  },
+  {
+    "revision": "83a78c7ee6a78417d807",
+    "url": "/jarvisinteraction/static/js/runtime~main.155fc5c2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jarvisinteraction/static/media/logo.5d5d9eef.svg"
+  }
+]);
